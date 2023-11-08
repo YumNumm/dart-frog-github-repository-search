df2fa2fab9721f7a0147489300a4775fcdb17684
Ryotaro Onoue <73390859+YumNumm@users.noreply.github.com>
2023-11-08 19:16:47 +0900

    fix: extension配置 (#18)

--- packages/bff/routes/api/v1/repository/search/index.dart
+++ packages/bff/routes/api/v1/repository/search/index.dart
@@ -1,8 +1,8 @@
 import 'package:bff_api_types/bff_api_types.dart' as types;
 import 'package:dart_frog/dart_frog.dart';
 import 'package:github_api/github_api.dart';
-import 'package:github_api/model/search_response.dart';
 
+import '../../../../../src/extension/repository_search_ex.dart';
 import '../../../../../src/util/method_check.dart';
 
 Future<Response> onRequest(RequestContext context) async {
@@ -25,44 +25,4 @@ Future<Response> onRequest(RequestContext context) async {
   return Response.json(
     body: result.toRepositorySearchResponse.toJson(),
   );
-}
-
-extension SearchResponseEx on SearchResponse {
-  types.RepositorySearchResponse get toRepositorySearchResponse {
-    return types.RepositorySearchResponse(
-      count: totalCount,
-      items: items
-          .map(
-            (e) => types.RepositorySearchResponseItem(
-              createdAt: e.createdAt,
-              description: e.description,
-              fork: e.fork,
-              forks: e.forks,
-              fullName: e.fullName,
-              htmlUrl: e.htmlUrl,
-              language: e.language,
-              mirrorUrl: e.mirrorUrl,
-              openIssues: e.openIssues,
-              owner: types.RepositorySearchResponseOwnder(
-                avatarUrl: e.owner.avatarUrl,
-                login: e.owner.login,
-                url: e.owner.url,
-              ),
-              private: e.private,
-              pushedAt: e.pushedAt,
-              topics: e.topics,
-              updatedAt: e.updatedAt,
-              watchers: e.watchers,
-              license: e.license != null
-                  ? types.RepositorySearchResponseLicense(
-                      key: e.license!.key,
-                      name: e.license!.name,
-                      url: e.license!.url,
-                    )
-                  : null,
-            ),
-          )
-          .toList(),
-    );
-  }
 }
